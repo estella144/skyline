972e3aa0e70e57d5e2fd37d5f702c5a4c796c5ad
Oliver <oliverng2512@icloud.com>
2023-03-30 21:06:32 +0100

    Finishing: 30.03.2023

--- picture.py
+++ picture.py
@@ -5,6 +5,7 @@ SKY_COLOR = "#00d0ff"
 BUILDING_1_COLOR = "#7d8e91"
 BUILDING_2_COLOR = "#aa4a44"
 DOOR_COLOR = "#ffffff"
+WINDOW_COLOR = "#00d0ff"
 
 logging_level = logging.DEBUG
 
@@ -47,7 +48,21 @@ Note: turtle will end with heading 90 (north)"""
         turtle.right(90)
 
     turtle.end_fill()
+
+def draw_window(turtle, height, start_x, start_y, color):
     
+    turtle.seth(90)
+    draw_rect(turtle, height, height, start_x, start_y, color, True)
+    draw_rect(turtle, height, height, start_x, start_y, "black", False)
+    turtle.forward(height/2)
+    turtle.right(90)
+    turtle.forward(height)
+    for i in range(2):
+        turtle.left(90)
+        turtle.forward(height/2)
+    turtle.left(90)
+    turtle.forward(height)
+
 def draw_bg(turtle):
     """Draws a frame for a picture."""
 
@@ -59,22 +74,34 @@ def draw_building_1(turtle):
 
 def draw_building_2(turtle, start_x, start_y):
 
-    draw_rect(turtle, 300, 400/5, start_x, start_y, BUILDING_2_COLOR, True)
+    draw_rect(turtle, 300, 400/5, start_x, start_y, DOOR_COLOR, True)
 
-def draw_house(turtle, start_x, start_y):
+def draw_house(turtle, start_x, start_y, i):
+
     draw_rect(turtle, 100, 400/4, start_x, start_y, BUILDING_2_COLOR, True)
-    draw_eq_tri(turtle, 100, 60, start_x, start_y + 100, BUILDING_2_COLOR, True)
+    if i != 200:
+        draw_eq_tri(turtle, 100, 60, start_x, start_y + 100, BUILDING_2_COLOR, True)
+    draw_rect(turtle, 30, 15, start_x+80/13+30/2, start_y, DOOR_COLOR, True)
+    draw_window(turtle, 30, start_x+30+80/3, start_y+40/3, WINDOW_COLOR)
+    draw_window(turtle, 30, start_x+30+80/3, start_y+40+40/3, WINDOW_COLOR)
+    draw_window(turtle, 30, start_x+80/3, start_y+40+40/3, WINDOW_COLOR)
     
 def draw_layer_2(turtle):
 
     draw_building_2(turtle, -140, -200)
     draw_building_2(turtle, 60, -200)
 
+def draw_layer_3(turtle):
+    
+    for i in range(-200, 300, 100):
+        draw_house(turtle, i, -200, i)
+
 def draw_picture(turtle):
     
     draw_bg(turtle)
     draw_building_1(turtle)
     draw_layer_2(turtle)
+    draw_layer_3(turtle)
     
 t = turtle.Turtle()
 t.speed(5)
